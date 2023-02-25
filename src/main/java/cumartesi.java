565f9e808cdca93107d89f531a7988523ee9e02f
Ümit Çelik <umitcelik.g@gmail.com>
2023-02-25 15:15:10 +0300

    v3

--- src/main/java/cumartesi.java
+++ src/main/java/cumartesi.java
@@ -2,6 +2,6 @@ public class cumartesi {
     public static void main(String[] args) {
 
         System.out.println("bugun gunlerden cumartyesi");
-        System.out.println("dun cumaydi");
+        System.out.println("Yarin gunlerden pazar");
     }
 }
